45ece089aae9dce02100526fbe5f04d868fdad66
Jetfred <105218522+Jetfred@users.noreply.github.com>
2022-05-12 15:58:18 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -11,6 +11,7 @@ input.onButtonPressed(Button.B, function () {
 input.onGesture(Gesture.Shake, function () {
     Compteur = Last_game
     basic.showNumber(Compteur)
+    basic.clearScreen()
     while (Compteur != 0) {
         Compteur += -1
         basic.pause(1000)
@@ -18,9 +19,11 @@ input.onGesture(Gesture.Shake, function () {
     }
     if (Compteur == 0) {
         soundExpression.giggle.play()
-        basic.showIcon(IconNames.Chessboard)
-        basic.showIcon(IconNames.Diamond)
-        basic.showIcon(IconNames.SmallDiamond)
+        while (true) {
+            basic.showIcon(IconNames.SmallDiamond)
+            basic.showIcon(IconNames.Diamond)
+            basic.showIcon(IconNames.Target)
+        }
     }
 })
 let Last_game = 0
@@ -29,3 +32,4 @@ basic.showString("321")
 Compteur = 0
 soundExpression.hello.play()
 soundExpression.hello.play()
+led.setBrightness(109)
