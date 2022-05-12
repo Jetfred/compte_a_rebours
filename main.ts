9d06cdfc24511244c2c79a5a2aa4595acc2199b9
Jetfred <105218522+Jetfred@users.noreply.github.com>
2022-05-12 15:27:04 +0200

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,12 +1,15 @@
 input.onButtonPressed(Button.A, function () {
     Compteur += 10
     basic.showNumber(Compteur)
+    Last_game = Compteur
 })
 input.onButtonPressed(Button.B, function () {
     Compteur += 5
     basic.showNumber(Compteur)
+    Last_game = Compteur
 })
 input.onGesture(Gesture.Shake, function () {
+    Compteur = Last_game
     basic.showNumber(Compteur)
     while (Compteur != 0) {
         Compteur += -1
@@ -18,8 +21,10 @@ input.onGesture(Gesture.Shake, function () {
         soundExpression.giggle.play()
     }
 })
+let Last_game = 0
 let Compteur = 0
 basic.showString("321")
 Compteur = 0
 soundExpression.hello.play()
 led.toggle(4, 4)
+soundExpression.hello.play()
